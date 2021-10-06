11ba499739b69f6fb20a4a00bb8a9667672b78f8
Manish Gaikwad <gaikawadmanish1@gmail.com>
2021-10-06 23:07:45 +0530

    Added npm crypto-js

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
+const SHA256 = require('crypto-js/sha256');
 class Block{
     constructor(index, timestamp, data, previousHash = ''){
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
+        this.hash = '';
+    }
+
+    calculateHash(){
     }
 }
